b67a06ff6bb2e8499f760519d5fe5c7d59dacaab
unknown <thomasiscooly@gmail.com>
2014-03-06 23:11:09 -0500

    added calsses for real this time

---
+++ src/jobs/Warrior.java
@@ -0,0 +1,15 @@
+package jobs;
+
+import textrpg.Player;
+
+public class Warrior 
+{
+    Player hero = new Player();
+    
+    public Warrior(Player p){hero = p;}//gets the player object
+    
+    public int attack()
+    {
+        return (int)(hero.getStrength() * .3) + 50;
+    }
+}
