6832c136eddaba0524f4dce8adb9c1f4144d806c
Konstantin Klymuk <katch.pr@gmail.com>
2014-04-05 23:22:17 +0300

    Added the scenario, as a test concept of prospective storyline

---
+++ rooms/ScenarioA.java
@@ -0,0 +1,50 @@
+package textrpg.rooms;
+
+/**
+ * User: katchk
+ * Date: 05.04.14
+ * Time: 18:10
+ */
+
+/*
+* In the future this one and another one scenarios will be
+* described in .txt or in other file format
+* and will be readed and created dynamically using
+* the JSON or other tools.
+* I think it will be cool, because other users will
+* be able to create and exchange their scenarios.
+* */
+public class ScenarioA {
+    Room startingRoom = new Room();
+    Room village = new Room();
+    Room wood = new Room();
+    Room lake = new Room();
+
+    public ScenarioA(){
+        startingRoom.setExits(village, null, null, null);
+        village.setExits(wood, startingRoom, lake, null);
+        wood.setExits(null, village, null, null );
+        lake.setExits(null, null, null, village);
+        setRoomsName();
+        setRoomsDescription();
+    }
+
+    private void setRoomsName(){
+        startingRoom.setRoomName("Training ground");
+        village.setRoomName("STARFIELD Village");
+        wood.setRoomName("The SHADOW GROVE");
+        lake.setRoomName("LAKE Of SOFIREAL");
+
+    }
+
+    private void setRoomsDescription(){
+        startingRoom.setRoomDescription("You are at training ground, near some village. Your adventure its begin!");
+        village.setRoomDescription("Gods forgotten place, near the SHADOW GROVE");
+        wood.setRoomDescription("Impregnated by rot and evilness air disturbs your mind...\nAnd it seems that you aren't alone here!");
+        lake.setRoomDescription("A warm breeze encircles your face ... and you fall in love with place");
+    }
+
+    public Room getStartingRoom(){
+        return startingRoom;
+    }
+}
