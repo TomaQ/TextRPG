31852cf32a168d369678298ab6694797da108fde
TomaQ <thomasiscooly@gmail.com>
2014-05-09 02:54:01 -0400

    formatting

--- weapons/IronSword.java
+++ weapons/IronSword.java
@@ -1,15 +1,14 @@
 package textrpg.weapons;
 
-public class IronSword extends Weapon
-{
-    public IronSword()
-    {
+public class IronSword extends Weapon {
+
+    public IronSword() {
         int[] s = {5, 0, 3, 0, 0, 0, 0};//testing not official {str, def, ?, ?, ?, ?, ?}
         super.setGoldWorth(50);
         super.setEquipmentType(1);
         super.setEquipmentStats(s);
         super.setName("Iron Sword");
-        
+
         super.setWeaponType(1);//testing not for realzz
         super.setWeaponDmg(9001);//lololz
     }
