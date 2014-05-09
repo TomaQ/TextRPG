cb61c25892fbaf0230e882932c4c10d4ecd110ca
TomaQ <thomasiscooly@gmail.com>
2014-05-09 02:47:31 -0400

    fixed comments and formatting

--- rooms/PowerRangerRoom.java
+++ rooms/PowerRangerRoom.java
@@ -3,10 +3,9 @@ package textrpg.rooms;
 import textrpg.npcs.DrSunshine;
 import textrpg.npcs.NPC;
 
-public class PowerRangerRoom extends Room
-{
-    public PowerRangerRoom(Room n, Room s, Room e, Room w)
-    {
+public class PowerRangerRoom extends Room {
+
+    public PowerRangerRoom(Room n, Room s, Room e, Room w) {
         super.setExits(n, s, e, w);
         super.setRoomName("Power Ranger Room");
         super.setRoomDescription("Mighty morphing power rangers! There's an old creep in the room.");
--- rooms/StartingRoom.java
+++ rooms/StartingRoom.java
@@ -4,10 +4,9 @@ import textrpg.items.*;
 import textrpg.shops.GeneralShop;
 import textrpg.shops.Shop;
 
-public class StartingRoom extends Room
-{    
-    public StartingRoom(Room n, Room s, Room e, Room w)
-    {
+public class StartingRoom extends Room {
+
+    public StartingRoom(Room n, Room s, Room e, Room w) {
         super.setExits(n, s, e, w);
         super.setRoomName("Starting Room");
         super.setRoomDescription("You are in a room, it feels like a starting room. There's a merchant sitting in the corner of the room.");
