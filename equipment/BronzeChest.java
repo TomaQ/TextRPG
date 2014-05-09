d35472a747e5cb4ad23b1897437023b973e63ca8
TomaQ <thomasiscooly@gmail.com>
2014-05-09 02:16:14 -0400

    formatting

--- equipment/BronzeChest.java
+++ equipment/BronzeChest.java
@@ -1,9 +1,8 @@
 package textrpg.equipment;
 
-public class BronzeChest extends Equipment
-{
-    public BronzeChest()
-    {
+public class BronzeChest extends Equipment {
+
+    public BronzeChest() {
         int[] stats = {5, 0, 2, 0, 0, 0, 0};//testing not official
         super.setGoldWorth(50);
         super.setEquipmentType(1);
--- equipment/Equipment.java
+++ equipment/Equipment.java
@@ -4,8 +4,7 @@ package textrpg.equipment;
 
 import textrpg.items.Item;
 
-public class Equipment extends Item
-{
+public class Equipment extends Item {
     private int equipmentType; //the type of equipment it is
     //1 = weapon, 2 = chest, 3 = legs, 4 = bracers, 5 = boots, 6 = gloves
     //7 = shield, 8 = offhand, 9 = ring, 10 = hat, 11 = goggles
