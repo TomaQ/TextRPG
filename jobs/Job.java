0d598395d446558ee5633f8987d8a5cafe8eecf6
unknown <thomasiscooly@gmail.com>
2014-06-19 01:28:21 -0400

    fix a magic number and formatted code

--- jobs/Job.java
+++ jobs/Job.java
@@ -9,36 +9,43 @@ import textrpg.skills.Skill;
 
 public class Job {
 
-    protected String jobName;
+    final int SUBSTRING_OFFSET = 2; //The offset for the length of a String that has ', ' added at the end
 
+    protected String jobName;
     private final String skills_package_path = "textrpg.skills.";
 
     protected List<Skill> skillsLearned;//the skills that the player currently has learned
     protected String[] availableSkills;//skills that the job is able to learn
     //ALL JOBS MUST LEARN ATTACK
-    
+
     public Job(){}//default
-    
+
     //Sets the players job
     public Job(Player h, int i) {
-        if(i == 1){h.setJob(new Warrior(h));}
-        else if(i == 2){h.setJob(new Mage(h));}
-        else{h.setJob(new Thief(h));}
+        if (i == 1) {
+            h.setJob(new Warrior(h));
+        }
+        else if (i == 2) {
+            h.setJob(new Mage(h));
+        }
+        else {
+            h.setJob(new Thief(h));
+        }
         //do stuff here with if's to check job and stuff
         //?? i need to write better comments
     }
-    
+
     public String getJobName(){return jobName;}
-    
+
     //Returns all of the skills learned as objects in an array
     public Skill[] getSkills() {
         Skill[] s = new Skill[skillsLearned.size()];
-        for(int i = 0; i < skillsLearned.size(); i++){
-                s[i] = skillsLearned.get(i);
+        for (int i = 0; i < skillsLearned.size(); i++) {
+            s[i] = skillsLearned.get(i);
         }
         return s;
     }
-    
+
     //Prints all of the skills learned
     public void printSkills() {
         Game.printBreak();
@@ -46,10 +53,10 @@ public class Job {
         for (int i = 0; i < skillsLearned.size(); i++) {
             skil += (skillsLearned.get(i).getSkillName() + ", ");
         }
-        skil = skil.substring(0, skil.length() - 2);
+        skil = skil.substring(0, skil.length() - SUBSTRING_OFFSET); //This removes the ', ' at the end of the string
         System.out.println(skil);
     }
-    
+
     //initializes all of the skills that are learned at level 1
     //This is temporary...
     public void initSkills(Player hero) throws ClassNotFoundException, InstantiationException, NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
@@ -60,5 +67,6 @@ public class Job {
             skillsLearned.add(skill);
         }
     }
-    public void levelUp(Player p){};//OVERWRITE THIS should make an interface for things like this or something
+
+    public void levelUp(Player p){};//OVERWRITE THIS should make an interface for things like this or something?
 }
