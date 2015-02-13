eb54d09ebbc03a688172c71ab997949121ec61fa
TomaQ <thomasiscooly@gmail.com>
2015-02-13 12:50:32 -0500

    modified engine class

--- Engine.java
+++ Engine.java
@@ -6,14 +6,21 @@ public class Engine {
 
     static Scanner scan = new Scanner(System.in);
     
+    private static void inputFormat() {
+        System.out.print(">");
+    }
+    
     public static String getString() {
+        inputFormat();
         String input = scan.nextLine();
         return input;
     }
     
     public static int getInt() {
+        inputFormat();
         int input = scan.nextInt();
         scan.nextLine();
         return input;
     }
+    
 }
